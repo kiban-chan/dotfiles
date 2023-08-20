99e65f78d7a3e81d1c027bfbdbd5a2a4c9658f65
SUGIHARA Kazuya <ksugihara.eng@gmail.com>
2023-08-20 23:13:51 +0900

    tmux.confを追加

--- mklink.sh
+++ mklink.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+# ディレクトリパスの取得
 repopath=$(cd $(dirname $0); pwd)
+
+# vimrc
 ln -sf $repopath/.vimrc $HOME/.vimrc
+
+# tmux.conf
+ln -sf $repopath/.tmux.conf $HOME/.tmux.conf
 
 # Vundleが無い場合にcloneする
 bundlepath=~/.vim/bundle/Vundle.vim/
