6439478a20fa68161993101edcbe7fe1ea15c424
SUGIHARA Kazuya <ksugihara.eng@gmail.com>
2024-01-01 01:22:57 +0900

    delete plugin installation

--- mklink.sh
+++ mklink.sh
@@ -9,13 +9,3 @@ ln -sf $repopath/.vimrc $HOME/.vimrc
 # tmux.conf
 ln -sf $repopath/.tmux.conf $HOME/.tmux.conf
 
-# Vundleが無い場合にcloneする
-bundlepath=~/.vim/bundle/Vundle.vim/
-if [ ! -e $bundlepath ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-else
-  echo "Vundle is already exist."
-fi
-
-# 以前の実施に関わらずvimのプラグインインストールを走らせる
-vim +PluginInstall +qall
